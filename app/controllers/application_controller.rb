263d74471cf63ef21cce8065266541b1856ba9d4
Evan Moore <etmoore@gmail.com>
2014-12-02 11:44:30 -0700

    pages#index action with tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :ensure_logged_in
+
+  def ensure_logged_in
+    redirect_to signin_path if session[:user_id].nil?
+  end
+
+  class AccessDenied < StandardError
+  end
+
+  rescue_from AccessDenied, with: :render_404
+
+  def render_404
+    render "public/404", status: :not_found, layout: false
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,15 @@
 class PagesController < ApplicationController
 
+  before_action :authorize
+
+  def index
+  end
+
+  private
+
+    def authorize
+      if session[:user_id] != 3
+        raise AccessDenied
+      end
+    end
 end
